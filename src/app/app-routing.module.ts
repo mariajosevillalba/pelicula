0cf494b794bd3822c33a69e367de3a133c41b269
Maria Alejandra Reyes Gonzalez <aleja.reyes0627@hotmail.com>
2021-11-18 19:42:16 -0500

    aplico bootstrap y meto un modelo de pelicula

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeliculasComponent } from './components/peliculas/peliculas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'peliculas', component:PeliculasComponent
+  },
+  {
+    path:'**', redirectTo:'',pathMatch:'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
